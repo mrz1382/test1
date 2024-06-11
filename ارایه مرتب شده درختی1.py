000675ab12384cbae2e7b15d8d709c4271eaff61
morteza <169180427+mrz1382@users.noreply.github.com>
2024-06-11 19:09:26 +0330

    Add files via upload

---
+++ ارایه مرتب شده درختی1.py
@@ -0,0 +1,35 @@
+def heapify(arr, n, i):
+    largest = i
+    l = 2 * i + 1
+    r = 2 * i + 2
+
+    if l < n and arr[i] < arr[l]:
+        largest = l
+
+    if r < n and arr[largest] < arr[r]:
+        largest = r
+
+    if largest != i:
+        arr[i], arr[largest] = arr[largest], arr[i]
+        heapify(arr, n, largest)
+
+def heap_sort(arr):
+    n = len(arr)
+
+    for i in range(n // 2 - 1, -1, -1):
+        heapify(arr, n, i)
+
+    for i in range(n-1, 0, -1):
+        arr[i], arr[0] = arr[0], arr[i]
+        heapify(arr, i, 0)
+
+# ورودی اعداد به صورت خام
+raw_input = input("please enter the number with space: ")
+arr = list(map(int, raw_input.split()))
+
+heap_sort(arr)
+
+# خروجی
+print("sorted array:")
+for num in arr:
+    print(num, end=' ')
